eb02f8030c51b529b7bc99895529716d2538be95
JJIMINSHIN <scalla1031@gmail.com>
2022-01-03 00:11:00 +0900

    commit

---
+++ SportsWear.py
@@ -0,0 +1,12 @@
+def solution(n, lost, reserve):
+    h_reserve = set(reserve) - set(lost)
+    h_lost = set(lost) - set(reserve)
+
+    for i in h_reserve:
+        if i-1 in h_reserve :
+            h_lost.remove(i-1)
+        elif i+1 in h_lost:
+            h_lost.remove(i+1)
+     
+    return n-len(h_lost)
+
